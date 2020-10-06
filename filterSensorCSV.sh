eaba3c654ba9c48ba6c80548db54780c2172cf9f
Chris Werner Rau <cwrau@cwrcoding.com>
2020-10-07 00:02:35 +0200

    Use single grep for better performance 😥

--- filterSensorCSV.sh
+++ filterSensorCSV.sh
@@ -10,23 +10,14 @@ inputCSV="${1:?You must provide the path to a csv}"
 tmpFilePrefix="tmp-$(basename $inputCSV)"
 
 function clean() {
-  find . -maxdepth 1 -type f -name "$tmpFilePrefix-*" -delete
+  rm -f "./$tmpFilePrefix-area.csv"
 }
 trap clean 0
 
-# split file for parallel processing
-split -u -d -n $(nproc) "$inputCSV" "./$tmpFilePrefix-grep-"
-
-# find sensors in area Bielefeld
-# the regex matches all ;lat;lon; that are in Bielefeld's bounding box (51.914,8.37,52.115,8.663 (https://wiki.openstreetmap.org/wiki/Bielefeld))
-find . -maxdepth 1 -type f -name "$tmpFilePrefix-grep-*" | xargs -t -r -P $(nproc) -i sh -c "grep --line-buffered -P ';5((1\.9(1([4-9]\d*)|([2-9]\d*))|([2-9]\d*))|2\.((1(1(([0-4]\d*)|5))|(0\d*))|(0\d*)));8\.((3((7\d*)|([8-9]\d*)))|([4-5]\d*)|(6((6(3|([0-2]\d*)))|([0-5]\d*))));' '{}' > './{}.csv'"
-
-#merge files
-find . -maxdepth 1 -type f -name "$tmpFilePrefix-grep-*.csv" -exec cat {} + > "./$tmpFilePrefix-area.csv"
+grep --line-buffered -P ';5((1\.9(1([4-9]\d*)|([2-9]\d*))|([2-9]\d*))|2\.((1(1(([0-4]\d*)|5))|(0\d*))|(0\d*)));8\.((3((7\d*)|([8-9]\d*)))|([4-5]\d*)|(6((6(3|([0-2]\d*)))|([0-5]\d*))));' "$inputCSV" > "./$tmpFilePrefix-area.csv"
 
 # filter unique locations for Bielefeld
-for entry in $(awk -F ';' '{if (!seen[$4+";"+$5]++) {print $0}}' "./$tmpFilePrefix-area.csv")
-do
+for entry in $(awk -F ';' '{if (!seen[$4+";"+$5]++) {print $0}}' "./$tmpFilePrefix-area.csv"); do
   lat=$(echo "$entry" | awk -F ';' '{print $4}')
   lon=$(echo "$entry" | awk -F ';' '{print $5}')
   city=$(curl -G --silent --data-urlencode "lat=$lat" --data-urlencode "lon=$lon" --data-urlencode "format=json" --data-urlencode "addressdetails=1" https://nominatim.openstreetmap.org/reverse | jq -r .address.city)
